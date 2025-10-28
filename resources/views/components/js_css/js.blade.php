d09d6c0594c851efd534a5d98bbfef3d75f717f3
ajmal <aanwari02@gmail.com>
2025-10-28 14:37:42 +0430

    working on localization

--- resources/views/components/js_css/js.blade.php
+++ resources/views/components/js_css/js.blade.php
@@ -3,6 +3,6 @@
 <script src="{{ asset('assets/js/bootstrap.min.js')}}"></script>
 <script src="{{ asset('assets/js/slick.min.js')}}"></script>
 <script src="{{ asset('assets/js/magnific-popup.min.js')}}"></script>
-<script src="{{ asset('assets/js/odometer.js')}}"></script>
+<script src="{{ asset('assets/js/odometer.min.js')}}"></script>
 <script src="{{ asset('assets/js/viewport.jquery.js')}}"></script>
 <script src="{{ asset('assets/js/main.js')}}"></script>
